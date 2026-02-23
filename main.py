a142a096af22b8fd281a388184b38f92c5401ce2
shozy-frenia <kozykorpesh2009@gmail.com>
2026-02-23 21:25:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from telegram.ext import (
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8450648107:AAFzBzyzRSizPj9ELbJcgK2y5mFxsxMu2tk"
 
 # States for Bayes calculator conversation
 (
